608ea4ad973fcb961a5685f879fb90188ecf8ff0
vicentepf78 <vicentepf78@gmail.com>
2021-05-22 15:16:42 -0300

    Cap 04 e 05

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -20,6 +20,32 @@ class NegociacaoController {
 
     }
 
+  
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+
+        service
+        .obterNegociacoes()
+        .then(negociacoes => {
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações do período importadas com sucesso';
+        })
+        .catch(error => this._mensagem.texto = error);  
+
+        /*Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes => {
+            negociacoes
+              .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+              .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        })
+        .catch(erro => this._mensagem.texto = erro); */
+
+    }
 
     adiciona(event) {
 
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -16,4 +16,8 @@ class ListaNegociacoes {
     esvazia() {
         this._negociacoes = [];
     }
+
+    get volumeTotal() {
+        return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+     }
 }
