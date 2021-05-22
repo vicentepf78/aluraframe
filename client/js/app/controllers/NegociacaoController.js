2178512d2797545897a287d40f6d68555b8cbcc9
Usuário <usuario@MacBook-Pro-de-Vicente.local>
2021-05-22 10:20:11 -0300

    Cap 03

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,19 +3,21 @@ class NegociacaoController {
     constructor() {
 
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._listaNegociacoes = new ListaNegociacoes(model => 
-            this._negociacoesView.update(model));
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia');
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto');
+
     }
 
 
@@ -25,7 +27,6 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         
         this._limpaFormulario();
 
@@ -35,8 +36,9 @@ class NegociacaoController {
     apaga() {
         this._listaNegociacoes.esvazia();
 
-        this._mensagemView.texto = 'Negociação apagadas com sucesso';
-        this._mensagemView.update(this._mensagemView);
+        this._mensagem.texto = 'Negociação apagadas com sucesso';
+        
+        this._limpaFormulario();
     }
 
 
